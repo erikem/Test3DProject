25f87ac060498aa0bf3c4e8eea374300817558f3
erikem <mistoff@gmail.com>
2022-06-01 22:45:30 +0300

    added comments to code

--- LevelObjects/Props/PassableProp.cs
+++ LevelObjects/Props/PassableProp.cs
@@ -5,19 +5,26 @@ using System.Collections.Generic;
 public class PassableProp : Spatial
 {
     protected List<string> _complimentaryProps = new List<string>();
+    //chance to spawn extra prop
     protected float _extraPropChance = 0.25f;
     protected static Random _rnd = new Random();
 
     virtual public void Init(Node rootScene, Spatial scenePropsContainer, float x, float z)
     {
+        //getting the object we'll be moving and rotating within tile
         var _propHolder = GetNode("Prop") as Spatial;
+        //defining the move distance and rotation
         float offsetX = x + (float)_rnd.NextDouble() * 0.8f + 0.1f;
         float offsetZ = z + (float)_rnd.NextDouble() * 0.8f + 0.1f;
         int offsetAngle = _rnd.Next(360);
+        //moving and rotating prop (has to be of scale 1)
         _propHolder.Translate(new Vector3(offsetX, 0, offsetZ));
         _propHolder.Rotate(Vector3.Up, Mathf.Deg2Rad(offsetAngle));
+        //CallDeferred is needed becaus the expect tree parent and root are not valid yet
         scenePropsContainer.CallDeferred("add_child", this);
         CallDeferred("set_owner", rootScene);
+        //we check if complimentary (extra) props list is not empty and check probability
+        //if passed we simply generate one more prop selected random from _complimentaryProps list
         if (_complimentaryProps.Count > 0
         && _rnd.NextDouble() < _extraPropChance)
         {
